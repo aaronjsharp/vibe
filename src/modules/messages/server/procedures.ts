8680c6546a2e2813eb0a74d797c3460515c11c8c
aaronjsharp <aaron.sharp2011@gmail.com>
2025-07-04 22:36:16 -0500

    09: Projects

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -17,12 +17,14 @@ export const messagesRouter = createTRPCRouter({
   create: baseProcedure
     .input(
       z.object({
-        value: z.string().min(1, { message: "Message is required" })
+        value: z.string().min(1, { message: "Message is required" }).max(10000, { message: "Value is too long"}),
+        projectId: z.string().min(1, { message: "Project ID is required"})
       })
     )
     .mutation(async ({ input }) => {
       const createdMessage = await prisma.message.create({
         data: {
+          projectId: input.projectId,
           content: input.value,
           role: "USER",
           type: "RESULT"
@@ -32,7 +34,8 @@ export const messagesRouter = createTRPCRouter({
       await inngest.send({
         name: "code-agent/run",
         data: {
-          value: input.value
+          value: input.value,
+          projectId: input.projectId
         }
       })
 
