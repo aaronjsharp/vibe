15695787a4b0b42deb9da193455256317c096709
aaronjsharp <aaron.sharp2011@gmail.com>
2025-07-07 14:47:27 -0500

    13: code view

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -8,16 +8,22 @@ import {
 import { MessagesContainer } from '../components/messages-container';
 import { Suspense, useState } from 'react';
 import { Fragment } from '@/generated/prisma';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { ProjectHeader } from '../components/project-header';
 import { FragmentWeb } from '../components/fragment-web';
+import { CodeIcon, CrownIcon, EyeIcon } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { FileExplorer } from '@/components/file-explorer';
 
 interface Props {
   projectId: string;
 }
 
 export const ProjectView = ({ projectId }: Props) => {
-  const [activeFragment, setActiveFragment] = useState<Fragment | null>(null)
-  
+  const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
+  const [tabState, setTabState] = useState<'preview' | 'code'>('preview');
+
   return (
     <div className='h-screen'>
       <ResizablePanelGroup direction='horizontal'>
@@ -29,22 +35,51 @@ export const ProjectView = ({ projectId }: Props) => {
           <Suspense fallback={<p>Loading project...</p>}>
             <ProjectHeader projectId={projectId} />
           </Suspense>
-          
+
           <Suspense fallback={<p>Loading messages...</p>}>
-            <MessagesContainer 
-              projectId={projectId} 
+            <MessagesContainer
+              projectId={projectId}
               activeFragment={activeFragment}
               setActiveFragment={setActiveFragment}
             />
           </Suspense>
-          
         </ResizablePanel>
         <ResizableHandle withHandle />
-        <ResizablePanel
-          defaultSize={50}
-          minSize={50}
-        >
-          {!!activeFragment && <FragmentWeb data={activeFragment} />}
+        <ResizablePanel defaultSize={50} minSize={50}>
+          <Tabs
+            className='h-full gap-y-0'
+            defaultValue='preview'
+            value={tabState}
+            onValueChange={(value) => setTabState(value as 'preview' | 'code')}
+          >
+            <div className='w-full flex items-center p-2 border-b gap-x-2'>
+              <TabsList className='h-8 p-0 border rounded-md'>
+                <TabsTrigger value='preview' className='rounded-md'>
+                  <EyeIcon /> <span>Demo</span>
+                </TabsTrigger>
+                <TabsTrigger value='code' className='rounded-md'>
+                  <CodeIcon /> <span>Code</span>
+                </TabsTrigger>
+              </TabsList>
+              <div className='ml-auto flex items-center gap-x-2'>
+                <Button asChild size='sm' variant='default'>
+                  <Link href='/pricing'>
+                    <CrownIcon /> Upgrade
+                  </Link>
+                </Button>
+              </div>
+            </div>
+            <TabsContent value='preview'>
+              {!!activeFragment && <FragmentWeb data={activeFragment} />}
+            </TabsContent>
+            <TabsContent value="code" className='min-h-0'>
+              {!!activeFragment?.files && (
+                <FileExplorer 
+                  files={activeFragment.files as { [path: string]: string}}
+                />
+              )}
+            </TabsContent>
+          </Tabs>
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
